0079ef1eec90327c720007e46661ba610514f800
EnglishBeach <kraynoff.i@yandex.ru>
2021-03-21 20:32:54 +0300

    Завершен передор сигм

--- Lessons/Random numbers.py
+++ Lessons/Random numbers.py
@@ -16,8 +16,10 @@ class Random_range():
         """Создание массива рандомных значений
 
         Args:
-            size (list, optional): [description]. Defaults to [2, 50].
+            sizeY (int, optional): [description]. Defaults to 2.
+            sizeX (int, optional): [description]. Defaults to 10.
             name ([type], optional): [description]. Defaults to None.
+            qual (int, optional): [description]. Defaults to 100.
         """
         self.qual = qual
         self.sizeX = sizeX
@@ -94,9 +96,10 @@ class Random_range():
         return suma
 
 
-for n in range(1):
-    a = Random_range(name=str(n), sizeX=5)
-
+print(' 2 - 4 - || ')
+for n in range(200):
+    a = Random_range(name=str(n), sizeX=50)
+    # Создание массива с нужными значениями. Эту часть можно улучшить и сделать красивой, но не успел еще.
     a.plus(0, 1)
     a.minus(0, 1)
 
@@ -117,12 +120,23 @@ for n in range(1):
 
     a.findsigma(2, 1)
     a.findsigma(3, 1)
-    
-    a.print()
-    print('Summa of {0} row {1: 5.2f}'.format(a.value[2][a.sizeX], a.summa(2)))
 
-    for s in range(1,a.sizeY//4):
-        this_range = a.value[s*4:s*4+4]
-        print(this_range)
+    # для проверки:
+    # a.print()
+    quality_indicator = [0, 0, 0, 0]
+    for s in range(1, 4):
+        r0 = a.summa(s * 4)
+        r1 = a.summa(s * 4 + 1)
+        rplus = a.summa(s * 4 + 2)
+        rminus = a.summa(s * 4 + 3)
+        # проверка
+        # print('%s %s %s %s'%(r0,r1,rplus,rminus))
 
-        print()
+        if rplus < r0 + r1 < rminus or rminus < r0 + r1 < rplus:
+            quality_indicator[s] = '+'
+        else:
+            quality_indicator[s] = ' '
+
+    print(' {0} | {1} | {2} '.format(quality_indicator[1],
+                                     quality_indicator[2],
+                                     quality_indicator[3]))
