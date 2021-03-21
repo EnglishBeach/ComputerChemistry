9a5455a87a6695038e4feed066ae760d84d888c8
EnglishBeach <kraynoff.i@yandex.ru>
2021-03-21 11:09:37 +0300

    Настроен вывод

--- Lessons/Random numbers.py
+++ Lessons/Random numbers.py
@@ -14,9 +14,9 @@ class Random_range():
             name ([type], optional): [description]. Defaults to None.
         """
         self.qual = qual
-        self.sizeX = sizeX + 1
+        self.sizeX = sizeX
         self.sizeY = sizeY
-        self.value = [[(rnd.random() * 100) // 1 for x in range(sizeX)]
+        self.value = [[((rnd.random() * 100) - 50) // 1 for x in range(sizeX)]
                       for y in range(sizeY)]
         for i in range(self.sizeY):
             self.value[i].append('R%s' % i)
@@ -24,47 +24,56 @@ class Random_range():
 
     def print(self):
 
-        print("%s's values" % self.name)
-        for i in range(self.sizeY):
-            print(self.value[i])
+        print("%s's values: " % self.name)
+        
+        for y in range(self.sizeY):
+            output = ''
+            for x in range(self.sizeX+1):
+                if type(self.value[y][x]) == str:
+                    output += ' |{0}'.format(self.value[y][x])
+                    
+                else:
+                    output += ' {0: >10.2e}'.format(self.value[y][x])
+                    
+            
+            print(output)
+    # print(self.value[i])
 
     def plus(self, row_one, row_two):
         answer = [(self.value[row_one][i] + self.value[row_two][i]) //
-                  (1 / self.qual) / self.qual for i in range(self.sizeX - 1)]
+                  (1 / self.qual) / self.qual for i in range(self.sizeX)]
         self.value.append(answer)
-        name = '%s + %s' % (self.value[row_one][self.sizeX - 1],
-                            self.value[row_two][self.sizeX - 1])
+        name = '%s + %s' % (self.value[row_one][self.sizeX],
+                            self.value[row_two][self.sizeX])
         self.value[self.sizeY].append(name)
         self.sizeY += 1
 
     def minus(self, row_one, row_two):
         answer = [(self.value[row_one][i] - self.value[row_two][i]) //
-                  (1 / self.qual) / self.qual for i in range(self.sizeX - 1)]
+                  (1 / self.qual) / self.qual for i in range(self.sizeX)]
         self.value.append(answer)
-        name = '%s - %s' % (self.value[row_one][self.sizeX - 1],
-                            self.value[row_two][self.sizeX - 1])
+        name = '%s - %s' % (self.value[row_one][self.sizeX],
+                            self.value[row_two][self.sizeX])
         self.value[self.sizeY].append(name)
         self.sizeY += 1
 
     def sigma(self, row, deg=2):
         suma = 0
-        for i in range(self.sizeX - 1):
+        for i in range(self.sizeX):
             suma += self.value[row][i]
         mid = suma / self.sizeX
         sigma = []
 
-        for i in range(self.sizeX - 1):
+        for i in range(self.sizeX):
             if deg == 1:
-                sigma.append(
-                    (abs(mid - self.value[row][i]) / self.sizeX - 1) //
-                    (1 / self.qual) / self.qual)
+                sigma.append((abs(mid - self.value[row][i]) / self.sizeX) //
+                             (1 / self.qual) / self.qual)
             else:
-                sigma.append(
-                    ((mid - self.value[row][i])**deg / self.sizeX - 1) //
-                    (1 / self.qual) / self.qual)
+                sigma.append(((mid - self.value[row][i])**deg / self.sizeX) //
+                             (1 / self.qual) / self.qual)
 
         self.value.append(sigma)
-        name = 'Si%s: %s' % (deg, self.value[row][self.sizeX - 1])
+        name = 'Si%s: %s' % (deg, self.value[row][self.sizeX])
         self.value[self.sizeY].append(name)
         self.sizeY += 1
 
@@ -73,7 +82,7 @@ class Random_range():
         #     self.value.append(suma)
 
 
-a = Random_range(name='First', sizeY=2)
+a = Random_range(name='First', sizeY=2,sizeX=10)
 a.plus(0, 1)
 a.sigma(0, 2)
 a.sigma(1, 2)
