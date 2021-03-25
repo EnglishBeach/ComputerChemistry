527b0db9a1780ccc404bc9e45d73c7ebaa54adff
EnglishBeach <kraynoff.i@yandex.ru>
2021-03-25 20:35:59 +0300

    FIX: Добавлены описания каждого метода и проги

--- Lessons/Random numbers.py
+++ Lessons/Random numbers.py
@@ -1,36 +1,58 @@
 import random as rnd
 """
-    Создает n раз массивы (представлены ниже) из случайных чисел, нужной длины и находит сигму (ниже Si-deg) в 2 и в 
-    других cтепенях (ниже deg), в том числе и в модуля, сравнивает находится ли значения 
-    Si(a) + Si(b) между Si(a+b) и Si(a-b), если да то помечает.
+    Создает массив из случайных значений А и В
+        A0 A1 A2 A3...
+        B0 B1 B2 B3...
+    Создает строку суммы и разности 2 выбранных строк
+    Считает функцию 'невязки' вида:
+            Si = (X-x)^deg/N, где X - среднее значение по строке, N - количество значений, n - степень 
+    Проверяет:  Si(A+B) < Si(A)+Si(B) < Si(A-B) или
+                Si(A-B) < Si(A)+Si(B) < Si(A+B)
+        Если да = то правило сложения выполняется => +
+        Если нет = то не выполняется => ' ' 
 
 """
 
 
 class Random_range():
-    """Создание класса для работы со случайными значениями
-    Этот класс сделан для тренировки, лучше использовать Pandas-frame для таких расчетов или хотя бы numpy. 
-    Но для тренировки так
     """
-    def __init__(self, sizeY=2, sizeX=10, name=None, qual=100):
-        """Создание массива рандомных значений
+    Создание массива случайных значений, с возможностью складывать/вычитать строки случайных сначений 1 из другой, находить функцию невязки для разных степеней.
+
+    Этот класс сделан для тренировки, лучше использовать Pandas-frame для таких расчетов или хотя бы numpy. 
+    """
+    def __init__(self,
+                 sizeY=2,
+                 sizeX=10,
+                 accuracy=100,
+                 normal=False,
+                 name=None):
+        """
+        Создает массив из случайных значений А и В
+        A0 A1 A2 A3...
+        B0 B1 B2 B3...
 
         Args:
-            sizeY (int, optional): [description]. Defaults to 2.
-            sizeX (int, optional): [description]. Defaults to 10.
-            name ([type], optional): [description]. Defaults to None.
-            qual (int, optional): [description]. Defaults to 100.
+            sizeY (int, optional): Количество строк массива с рандомными значениями. Defaults to 2.
+            sizeX (int, optional): Длина строки, фактически размер выборки 1-ой переменной. Defaults to 10.
+            accuracy (int, optional): Количество знаков после . случайного числа. Ограничивает огромные хвосты в расчетах или выводе. Defaults to 100.
+            normal (byl, optional): Условие нормировки значений (деление на размах выборки). Defaults to False.
         """
-        self.qual = qual
+        self.name = name
+        self.normal = normal
+        self.accuracy = accuracy
         self.sizeX = sizeX
         self.sizeY = sizeY
-        self.value = [[((rnd.random() * 100) - 50) // 1 for x in range(sizeX)]
-                      for y in range(sizeY)]
+        if normal == True: nkoef = 1
+        else: nkoef = 100
+        self.value = [[((rnd.random() * nkoef) - nkoef // 2) // 1
+                       for x in range(sizeX)] for y in range(sizeY)]
         for i in range(self.sizeY):
             self.value[i].append('R%s' % i)
-        self.name = name
 
     def print(self):
+        """
+        Выводит на экран значения всего массива
+        """
 
         print("%s's values: " % self.name)
 
@@ -46,8 +68,16 @@ class Random_range():
             print(output)
 
     def plus(self, row_one, row_two):
+        """
+        Создает строку суммы 2 выбранных строк
+
+        Args:
+            row_one ([type]): 1 строка
+            row_two ([type]): 2 строка
+        """
         answer = [(self.value[row_one][i] + self.value[row_two][i]) //
-                  (1 / self.qual) / self.qual for i in range(self.sizeX)]
+                  (1 / self.accuracy) / self.accuracy
+                  for i in range(self.sizeX)]
         self.value.append(answer)
         name = '%s + %s' % (self.value[row_one][self.sizeX],
                             self.value[row_two][self.sizeX])
@@ -55,8 +85,16 @@ class Random_range():
         self.sizeY += 1
 
     def minus(self, row_one, row_two):
+        """
+        Создает строку разности 2 выбранных строк 1строка - 2строка
+
+        Args:
+            row_one ([type]): 1 строка
+            row_two ([type]): 2 строка
+        """
         answer = [(self.value[row_one][i] - self.value[row_two][i]) //
-                  (1 / self.qual) / self.qual for i in range(self.sizeX)]
+                  (1 / self.accuracy) / self.accuracy
+                  for i in range(self.sizeX)]
         self.value.append(answer)
         name = '%s - %s' % (self.value[row_one][self.sizeX],
                             self.value[row_two][self.sizeX])
@@ -64,11 +102,17 @@ class Random_range():
         self.sizeY += 1
 
     def findsigma(self, row, deg=2):
-        """Возвращает дисперсию степени deg. 1 - для модуля
+        """
+        Считает функцию 'невязки' вида:
+            Si = (X-x)^deg/N, где X - среднее значение по строке, N - количество значений, n - степень 
+            (n=2 - обычная дисперсия), n=1 - модуль числа
 
         Args:
             row (int) Номер строки
-            deg (int, optional): [description]. Defaults to 2.
+            deg (int, optional): Степень в функции 'невязки'. Неустойчива к четным дробным (вызовет ошибку). 
+                = 2 - обычная дисперсия. 
+                = 1 - модуль
+                Defaults to 2.
         """
         suma = 0
         for i in range(self.sizeX):
@@ -79,10 +123,10 @@ class Random_range():
         for i in range(self.sizeX):
             if deg == 1:
                 sigma.append((abs(mid - self.value[row][i]) / self.sizeX) //
-                             (1 / self.qual) / self.qual)
+                             (1 / self.accuracy) / self.accuracy)
             else:
                 sigma.append(((mid - self.value[row][i])**deg / self.sizeX) //
-                             (1 / self.qual) / self.qual)
+                             (1 / self.accuracy) / self.accuracy)
 
         self.value.append(sigma)
         name = 'Si%s: %s' % (deg, self.value[row][self.sizeX])
@@ -97,9 +141,10 @@ class Random_range():
 
 
 print(' 2 - 4 - || ')
+#Можно сделать лучше вывод, но это трудоемко и не столь важно
 for n in range(200):
-    a = Random_range(name=str(n), sizeX=50)
     # Создание массива с нужными значениями. Эту часть можно улучшить и сделать красивой, но не успел еще.
+    a = Random_range(name=str(n), sizeX=10)
     a.plus(0, 1)
     a.minus(0, 1)
 
@@ -123,12 +168,17 @@ for n in range(200):
 
     # для проверки:
     # a.print()
+
+    # Индикатор сходимости для каждой степени
     quality_indicator = [0, 0, 0, 0]
+
+    # Пробелается по каждому блоку из вычесленных Si для каждой степени
     for s in range(1, 4):
         r0 = a.summa(s * 4)
         r1 = a.summa(s * 4 + 1)
         rplus = a.summa(s * 4 + 2)
         rminus = a.summa(s * 4 + 3)
+
         # проверка
         # print('%s %s %s %s'%(r0,r1,rplus,rminus))
 
@@ -136,7 +186,6 @@ for n in range(200):
             quality_indicator[s] = '+'
         else:
             quality_indicator[s] = ' '
-
     print(' {0} | {1} | {2} '.format(quality_indicator[1],
                                      quality_indicator[2],
                                      quality_indicator[3]))
