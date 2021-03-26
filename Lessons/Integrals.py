a103064d375aab658d106c7c904c19195b56bc78
EnglishBeach <kraynoff.i@yandex.ru>
2021-03-26 11:29:58 +0300

    NEW: Метод Котеса и Трапеций добавлены

--- Lessons/Integrals.py
+++ Lessons/Integrals.py
@@ -44,7 +44,7 @@ def monte_krist_graph(a, b, maxf, max_steps=10000):
     return area * nsucces / max_steps
 
 
-def monte_krist_analyse(a, b, max_steps=10000):
+def monte_krist_analyse(a, b, max_steps=10):
     """Вычисляет интеграл методом Монте-Карло. Аналитический алгорим: основан на свойстве определенного интеграла 
     (I = (b-a)f(e), е- некоторое число на промежутке)
 
@@ -60,13 +60,48 @@ def monte_krist_analyse(a, b, max_steps=10000):
     for i in range(max_steps):
         xr = a + (b - a) * rnd.random()
         s += f(xr)
-    return s / max_steps*(b-a)
-
-def trap (a, b, quality=1, max_steps=10000)
+    return s / max_steps * (b - a)
 
 
+def trap(a, b, max_steps=10000):
+    dx = (b - a) / max_steps
+    s = 0
+    for i in range(max_steps):
+        s += (f(a + dx * i) + f(a + dx * (i + 1)))
+    return s / 2 * dx
 
-a = monte_krist_analyse(0, 2)
+
+def kotes(a, b, n=3):
+    dx = (b - a) / n
+    s = 0
+    A = []
+    for i in range(8):
+        A.append(0)
+
+    if n == 1:
+        A[0] = 1
+        A[1] = 1
+        N = 2
+    elif n == 2:
+        A[0] = 1
+        A[1] = 4
+        A[2] = 1
+        N = 6
+    elif n == 3:
+        A[0] = 1
+        A[1] = 3
+        A[2] = 3
+        A[3] = 1
+        N = 8
+    suma = 0
+    for i in range(8):
+        suma += A[i] * f(a + dx * i)
+
+    return n * dx * suma / N
+
+
+a = monte_krist_analyse(0, 2, max_steps=10000)
 b = monte_krist_graph(0, 2, 10)
-print(a,b)
-
+c = trap(0, 2)
+d = kotes(0,2)
+print(a, b, c, d)
