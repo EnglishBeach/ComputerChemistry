e1770216fba8edd3b13b6dd86dd43d18d31739bf
EnglishBeach <kraynoff.i@yandex.ru>
2021-03-26 10:58:54 +0300

    NEW: Монте-Карло добавлены

--- Lessons/Integrals.py
+++ Lessons/Integrals.py
@@ -1,4 +1,6 @@
 import random as rnd
+
+
 def print2D(f):
     """
     Выводит на экран значения всего массива
@@ -14,16 +16,54 @@ def print2D(f):
                 output += ' {0: >8.2f}'.format(f[y][x])
         print(output)
 
+
 def f(x):
     """Уравнение для нахождения корня его f(x) = 0
     """
-    return x**2 + 2 * x - 3
+    return x**2
 
 
-def monte_krist_graph (a, b,maxf, quality=1, max_steps=100):
-    area = maxf*(b-a)
+def monte_krist_graph(a, b, maxf, max_steps=10000):
+    """Вычисляет интеграл методом Монте-Карло. Графический алгорим: основан на вероятности попадания случайного чила в область под графиком
+
+    Args:
+        a (float): Левая граница
+        b (float): Правая граница
+        maxf (float): верхняя граница
+        max_steps (int, optional): Максимальное число шагов. Defaults to 10000.
+
+    Returns:
+        float: Значение интеграла
+    """
+    area = maxf * (b - a)
+    nsucces = 0
     for i in range(max_steps):
-        xr = a+(b-a)*rnd.random()
+        xr = a + (b - a) * rnd.random()
         yr = maxf * rnd.random()
-        f 
+        if f(xr) > yr: nsucces += 1
+    return area * nsucces / max_steps
+
+
+def monte_krist_analyse(a, b, quality=1, max_steps=10000):
+    """Вычисляет интеграл методом Монте-Карло. Аналитический алгорим: основан на свойстве определенного интеграла 
+    (I = (b-a)f(e), е- некоторое число на промежутке)
+
+    Args:
+        a (float): Левая граница
+        b (float): Правая граница
+        max_steps (int, optional): Максимальное число шагов. Defaults to 10000.
+
+    Returns:
+        float: Значение интеграла
+    """
+    s = 0
+    for i in range(max_steps):
+        xr = a + (b - a) * rnd.random()
+        s += f(xr)
+    return s / max_steps*(b-a)
+
+# def trap
+a = monte_krist_analyse(0, 2)
+b = monte_krist_graph(0, 2, 10)
+print(a,b)
 
