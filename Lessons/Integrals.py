165de4519bc134b36a6779097b178ba6f220d38b
EnglishBeach <kraynoff.i@yandex.ru>
2021-03-27 21:26:53 +0300

    NEW: Начата настройка методов под модули

--- Lessons/Integrals.py
+++ Lessons/Integrals.py
@@ -1,12 +1,9 @@
 import random as rnd
 
-def f(x):
-    """Уравнение для нахождения корня его f(x) = 0
-    """
-    return x**2
+# f = lambda x: x**2
 
 
-def monte_krist_graph(a, b, maxf, max_steps=10000):
+def monte_krist_graph(a, b, maxf,f, max_steps=10000):
     """Вычисляет интеграл методом Монте-Карло. Графический алгорим: основан на вероятности попадания случайного чила в область под графиком
 
     Args:
@@ -26,98 +23,100 @@ def monte_krist_graph(a, b, maxf, max_steps=10000):
     return area * nsucces / max_steps
 
 
-def monte_krist_analytical(a, b, max_steps=10):
-    """Вычисляет интеграл методом Монте-Карло. Аналитический алгорим: основан на свойстве определенного интеграла 
-        I = (b-a)f(e), 
-        е - некоторое число на промежутке [a;b], f(e) - среднее значение f(x)
+# def monte_krist_analytical(a, b, max_steps=10):
+#     """Вычисляет интеграл методом Монте-Карло. Аналитический алгорим: основан на свойстве определенного интеграла 
+#         I = (b-a)f(e), 
+#         е - некоторое число на промежутке [a;b], f(e) - среднее значение f(x)
 
-    Args:
-        a (float): Левая граница
-        b (float): Правая граница
-        max_steps (int, optional): Максимальное число шагов. Defaults to 10000.
-    Returns:
-        (float): Значение интеграла
-    """
-    s = 0
-    for i in range(max_steps):
-        xr = a + (b - a) * rnd.random()
-        s += f(xr)
-    return s / max_steps * (b - a)
-
-
-def trap(a, b, max_steps=10000):
-    """Вычисляет интеграл методом трапеций
-
-    Args:
-        a (float): Левая граница
-        b (float): Правая граница
-        max_steps (int, optional): Максимальное число шагов. Defaults to 10000.
-    Returns:
-        (float): Значение интеграла
-    """
-    dx = (b - a) / max_steps
-    s = 0
-    for i in range(max_steps):
-        s += (f(a + dx * i) + f(a + dx * (i + 1)))
-    return s / 2 * dx
+#     Args:
+#         a (float): Левая граница
+#         b (float): Правая граница
+#         max_steps (int, optional): Максимальное число шагов. Defaults to 10000.
+#     Returns:
+#         (float): Значение интеграла
+#     """
+#     s = 0
+#     for i in range(max_steps):
+#         xr = a + (b - a) * rnd.random()
+#         s += f(xr)
+#     return s / max_steps * (b - a)
 
 
-def kotes(a, b, n=3):
-    """Вычисляет интеграл методом Котеса
+# def trap(a, b, max_steps=10000):
+#     """Вычисляет интеграл методом трапеций
 
-    Args:
-        a (float): Левая граница
-        b (float): Правая граница
-        n (int, optional): Число Котеса.
-    Returns:
-        (float): Значение интеграла
-    """
-    dx = (b - a) / n
-    s = 0
-    A = []
-    for i in range(8):
-        A.append(0)
+#     Args:
+#         a (float): Левая граница
+#         b (float): Правая граница
+#         max_steps (int, optional): Максимальное число шагов. Defaults to 10000.
+#     Returns:
+#         (float): Значение интеграла
+#     """
+#     dx = (b - a) / max_steps
+#     s = 0
+#     for i in range(max_steps):
+#         s += (f(a + dx * i) + f(a + dx * (i + 1)))
+#     return s / 2 * dx
 
-    if n == 1:
-        A[0] = 1
-        A[1] = 1
-        N = 2
-    elif n == 2:
-        A[0] = 1
-        A[1] = 4
-        A[2] = 1
-        N = 6
-    elif n == 3:
-        A[0] = 1
-        A[1] = 3
-        A[2] = 3
-        A[3] = 1
-        N = 8
-    suma = 0
-    for i in range(8):
-        suma += A[i] * f(a + dx * i)
 
-    return n * dx * suma / N
+# def kotes(a, b, n=3):
+#     """Вычисляет интеграл методом Котеса
 
-x0 = -9
-x1 = 15
-print('         Integrals %s to %s:    ' % (x0,x1))
-print('    Monte-Karlo, analytical:')
-a = monte_krist_analytical(x0, x1, max_steps=10000)
-print(a)
-print()
+#     Args:
+#         a (float): Левая граница
+#         b (float): Правая граница
+#         n (int, optional): Число Котеса.
+#     Returns:
+#         (float): Значение интеграла
+#     """
+#     dx = (b - a) / n
+#     s = 0
+#     A = []
+#     for i in range(8):
+#         A.append(0)
 
-print('    Monte-Karlo, graphical:')
-b = monte_krist_graph(x0, x1, 100)
-print(b)
-print()
+#     if n == 1:
+#         A[0] = 1
+#         A[1] = 1
+#         N = 2
+#     elif n == 2:
+#         A[0] = 1
+#         A[1] = 4
+#         A[2] = 1
+#         N = 6
+#     elif n == 3:
+#         A[0] = 1
+#         A[1] = 3
+#         A[2] = 3
+#         A[3] = 1
+#         N = 8
+#     suma = 0
+#     for i in range(8):
+#         suma += A[i] * f(a + dx * i)
 
-print('    Trapezoids: ')
-c = trap(x0, x1)
-print(c)
-print()
+#     return n * dx * suma / N
 
-print('    Kotes: ')
-d = kotes(x0, x1)
-print(d)
-print()
+if __name__ == '__main__':
+    x0 = -9
+    x1 = 15
+    print('         Integrals %s to %s:    ' % (x0,x1))
+    print('    Monte-Karlo, analytical:')
+    a = monte_krist_analytical(x0, x1, max_steps=10000)
+    print(a)
+    print()
+
+    print('    Monte-Karlo, graphical:')
+    b = monte_krist_graph(x0, x1, 100)
+    print(b)
+    print()
+
+    print('    Trapezoids: ')
+    c = trap(x0, x1)
+    print(c)
+    print()
+
+    print('    Kotes: ')
+    d = kotes(x0, x1)
+    print(d)
+    print()
+    print()
