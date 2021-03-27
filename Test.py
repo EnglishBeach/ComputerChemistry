901f46c5d7a2a17fef6325016c90bfb5d21e6068
EnglishBeach <kraynoff.i@yandex.ru>
2021-03-27 21:26:53 +0300

    NEW: Начата настройка методов под модули

--- Test.py
+++ Test.py
@@ -1,20 +1,5 @@
-# class Random_range(object):
-#     def __init__
+import importlib
 
+INT = importlib.import_module('Lessons.Integrals')
 
-class Vehicle(object):
-    def __init__(self, size, color='Black', material='Steel'):
-        self.color = color
-        self.material = material
-        self.size = size
-
-    def broke(self):
-        return 'Broken %s car maden from %s' % (self.size, self.material)
-
-    def drive(self):
-        return 'Driving %s %s car right now!' % (self.size, self.color)
-
-
-inpsize = input("Input Vehicles's size: ")
-car1 = Vehicle(size=inpsize, color='Red', material='Wood')
-print(type(car1))
+INT.
