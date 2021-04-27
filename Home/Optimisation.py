8d1af3b7d299c9ceda2fdbada2649588e85a9444
EnglishBeach <80720628+EnglishBeach@users.noreply.github.com>
2021-04-27 13:43:00 +0300

    FIX: Исправление вычсления градиента

--- Home/Optimisation.py
+++ Home/Optimisation.py
@@ -2,50 +2,69 @@ import sympy as sym
 import sympy.matrices as mat
 from sympy.plotting import plot3d
 
-x, y = sym.symbols('x y')
+(x,y,z)=sym.symbols('x:z')
+h = sym.symbols('h')
+
+# Сама функция:
 # f = sym.sin(sym.sqrt(x**2 + y**2))
 f = x**2 + y**2
-feval = sym.lambdify([x, y], f)
 
+# Условие для ограничения:
+g = 4-x-y
+
+# Начальные параметры:
 X0, Y0 = 3.5, 3.5
 
+# Технические параметры
 MAXI = 500
 STEP = 1
 
+# Диапазон вывода графика
 XDIAP = [-10, 10]
 YDIAP = [-10, 10]
 
 
 def create_gauss(f):
-    H = mat.zeros(2, 2)
-    fx = f.diff(x)
-    fy = f.diff(y)
-    H[0, 0] = fx.diff(x)
-    H[0, 1] = fx.diff(y)
-    H[1, 0] = fy.diff(x)
-    H[1, 1] = fy.diff(y)
+    """Вычисляет матрицу Якоби для f 2 переменных
+
+    Args:
+        f (sympy.function): Функиця
+
+    Returns:
+        Якобиан
+    """
+    H = [[f.diff(x).diff(y) for x in (x,y,z)] for y in (x,y,z)]
+    H = mat.Matrix(H)
     return H
 
 
-def optimise_Newton(f):
-    x0 = X0
-    y0 = Y0
+def optimise_FastDown(f):
+    """Вычисляет min мметодом наискорейшего спуска
 
-    H = create_gauss(f)
-    s = sym.lambdify([x, y], -H.det())
+    Args:
+        f (sympy.function): Функция
+    """
+    feval = sym.lambdify([x, y], f)
+    # Начальная точка
+    r0 = mat.Matrix((X0,Y0))
 
-    P = mat.Matrix(1, 2, [x0, y0])
+    # Создание градиента 
+    g= mat.Matrix([f.diff(x) for x in (x,y)])
+
+    p = mat.Matrix()
 
     i = 0
-
+    r1 =mat.Matrix()
     while True:
         i += 1
-        x1 = x0 + STEP * s(x0, y0)
-        y1 = y0 + STEP * s(x1, y0)
+
+        r1 = r0 - STEP * g.subs({x:})
+
         X1 = mat.Matrix(1, 2, [x1, y1])
         if (feval(x0,y0) - feval(x1,y1)) < STEP or i > MAXI: break
-        P = P.row_insert(99999, X1)
-        x0,y0=x1,y1
+
+        p = p.row_insert(99999, r1.transpose())
+        r0 = r1
 
     sym.pprint(P)
     print('Steps: %s'%i)
@@ -53,7 +72,13 @@ def optimise_Newton(f):
 
 # def optimise_Monte_karlo(f):
 
-# def optimase
+def optimase_if(f):
+    F = f - h* g
+
+optimise_FastDown(f)
 
 
-optimise_Newton(f)
+
+
+
+# optimise_FastDown(f)
