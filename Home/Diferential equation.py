ba612991d298d4d718341519e84048eb9277c3ba
EnglishBeach <80720628+EnglishBeach@users.noreply.github.com>
2021-04-27 18:08:37 +0300

    NEW: Runge сделан

--- Home/Diferential equation.py
+++ Home/Diferential equation.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import math as mt
 
-f=  lambda x,y:  mt.sin(x)
+f=  lambda x,y:  x
 X0 = 1
 Y0 = 0
-STEPS = 1000
+STEPS = 10
 DX = 0.2
 
 def eiler(f, x, y, steps, dx=0.1):
@@ -38,47 +38,58 @@ def runge(f, x, y, steps, dx=0.1,type = 1):
     k3 = dx*f(x+dx,y+2*k2 - k1)
     k4= dx*f(x+dx,y+k3)
 
+    Xlist = [x]
+    Ylist = [y]
+    
     if type == 1:
-        return eiler(x, x, y, steps, dx=0.1)
-    elif type == 2:
-        y1 =y+ dx*f(k1+4*k2+k3)
-        return f(k1+4*k2+k3)
-    elif type == 3:
-        k1= dx*f(x,y)
-        k2= dx*f(x+dx/2,y+k1/2)
-        k3 = dx*f(x+dx,y+2*k2 - k1)
-
-        y1 =y+ dx*f(k1+4*k2+k3)
-        return f(k1+4*k2+k3)
-    elif type == 4:
-        k1= dx*f(x,y)
-        k2= dx*f(x+dx/2,y+k1/2)
-        k3 = dx*f(x+dx,y+k2/2)
-        k4= dx*f(x+dx,y+k3)
-
-    for i in range(steps):
+        return eiler(f,x,y, steps, dx)
         
+    elif type == 2:
+        for i in range(steps):
+            y1 = y+dx*f(x,y)/2
+            y = y+dx*f(x+dx/2,y1) 
+            x+=dx
 
-        y1 =y+ dx*(k1+2*k2+2*k3+k4)/6
+            Xlist.append(x)
+            Ylist.append(y)    
+        return [Xlist,Ylist]
+      
+    elif type == 3:
+        for i in range(steps):
+            k1= dx*f(x,y)
+            k2= dx*f(x+dx/2,y+k1/2)
+            k3 = dx*f(x+dx,y+2*k2 - k1)
+            y =y+ dx*(k1+4*k2+k3)
+            x+=dx
 
-    ynext = y + dx * AF(x, y)
-    x += dx
-    Y.append(ynext)
-    X.append(x)
-    y = ynext
+            Xlist.append(x)
+            Ylist.append(y)    
+        return [Xlist,Ylist]
+        
+    elif type == 4:
+        for i in range(steps):
+            k1= dx*f(x,y)
+            k2= dx*f(x+dx/2,y+k1/2)
+            k3 = dx*f(x+dx,y+k2/2)
+            k4= dx*f(x+dx,y+k3)
 
+            y =y+ dx*(k1+2*k2+2*k3+k4)/6
+            x+=dx
 
-
+            Xlist.append(x)
+            Ylist.append(y)    
+        return [Xlist,Ylist]        
+        
 def main(f):
     # Решение уравнения dy/dx = F(x,y)
 
-    answer = eiler(f, x=X0, y=Y0, steps=STEPS, dx=-DX)
+    answer = runge(f, x=X0, y=Y0, steps=STEPS, dx=-DX)
     X = answer[0]
     Y = answer[1]
     X.reverse()
     Y.reverse()
 
-    answer = eiler(f, x=X0, y=Y0, steps=STEPS, dx=DX)
+    answer = runge(f, x=X0, y=Y0, steps=STEPS, dx=DX)
     X.extend(answer[0])
     Y.extend(answer[1])
 
