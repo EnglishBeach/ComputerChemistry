d565fbdd8deb200bf45455291004925087c4cd31
EnglishBeach <kraynoff.i@yandex.ru>
2021-05-03 14:03:28 +0300

    FIX: Вопрос по числам

--- Class/Concentrat.py
+++ Class/Concentrat.py
@@ -1,3 +1,4 @@
+# %%
 import math as mt
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,31 +12,32 @@ K = 1
 
 # Входные данные
 T0 = 300
-T1 = 400
+T1 = 450
 
-QUALITY = 0.1
+QUALITY = 0.01
 
 TIME0 = 0
 TIME1 = 600
 TIME_N = 10
 MAXTIMEPOINTS = 32
 
-
+# %%
 class Time_function:
     """Функция от 1 переменной с параметрами
     """
-    def __init__(self, T0, T1) -> None:
+    def __init__(self, T0, T1,k0) -> None:
         """Constructor"""
         self.T0 = T0
         self.T1 = T1
+        self.k0=k0
 
     def __call__(self, t):
         temp = lambda t: self.T0 + (self.T1 - self.T0) * (1 - mt.exp(
             -t / SPEED_CHANGE_T))
-        k = lambda t: K * mt.exp(-E / (KB * temp(t)))
+        k = lambda t: self.k0 * mt.exp(-E / (KB * temp(t)))
         return k(t)
 
-
+# %%
 def kotes(f, a, b, n=3):
     """Вычисляет интеграл методом Котеса
     Args:
@@ -76,29 +78,31 @@ def kotes_quality(f, a, b, quality=0.1, n=3):
     i = 0
     while True:
         x_n = 4 * 2**i
-        dx = (a - b) / x_n
+        dx = (b - a) / x_n
         s = 0
         for nx in range(x_n):
             s += kotes(f, a=a + dx * nx, b=a + dx * (nx + 1), n=n)
-        if abs((s - s0) / s0) < quality and x_n > MAXTIMEPOINTS: return s
+        if abs((s - s0) / s0) < quality or x_n > MAXTIMEPOINTS: return s
         i += 1
 
-
+# %%
 def main():
-    func_k = Time_function(T0, T1)
-    data = pd.DataFrame({'Temperature:': [TIME0], 'Concentration:': [C0]})
+    func_k = Time_function(T0, T1,K)
+    data = pd.DataFrame({'Time:': [TIME0], 'Concentration:': [C0]})
 
     # Перебор времени
     dtime = (TIME1 - TIME0) / TIME_N
     for ntime in range(TIME_N):
         a_time = TIME0 + dtime * ntime
         b_time = TIME0 + dtime * (ntime + 1)
-        k = kotes_quality(func_k, a=a_time, b=b_time, quality=QUALITY, n=2)
-
-        data = data.append({
-            'Concentrations': [C0 * mt.exp(-k)],
-            'Temperatures': [b_time]
-        })
+        summ_k = kotes_quality(func_k, a=a_time, b=b_time, quality=QUALITY, n=2)
+        # func_k.k0=k
+        data = data.append(
+            {
+                'Concentration:': C0 * mt.exp(-summ_k),
+                'Time:': b_time
+            },
+            ignore_index=True)
 
     print(data)
     # plt.figure(figsize=(16, 10), dpi=80, facecolor='w', edgecolor='k')
@@ -106,5 +110,5 @@ def main():
     # plt.grid(True)
     # plt.show()
 
-
+# %%
 main()
